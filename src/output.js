12e7ee8c92d52c75f5d94fdf06f701920f969b30
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-11-04 00:29:39 +1100

    Inital release

--- src/output.js
+++ src/output.js
@@ -8,7 +8,7 @@ module.exports = {
      * Print the main hw configuration
      */
     print: function () {
-        console.info(`${core.hostname()}
+    	console.info(`${core.hostname()}
 	  Description: Computer
 	  Width: ${core.arch().arch}\t
         *-core\t
@@ -20,13 +20,12 @@ module.exports = {
                 Product: ${core.cpu().product}
                 Vendor: ${core.cpu().vendor}
                 Width: ${core.cpu().width}
-        *-network`)
-        Object.entries(core.network().interfaces).forEach( item => {
-          console.info(`\t  Description: Network interface
-          Logical Name: ${item[0]}
-          Serial: ${item[1][0].mac}
-          Configuration: ip=${item[1][0].address}`)
-        });
+		*-network\t
+		${Object.entries(core.network().interfaces).map( item => {
+			return `    Description: Network interface
+		    Logical Name: ${item[0]}
+		    Serial: ${item[1][0].mac}
+		    Configuration: ip=${item[1][0].address}\n\t\t`}).join("")}`);
     },
     /**
      * Print the module version
@@ -35,36 +34,134 @@ module.exports = {
         console.info(`lshw ${packageJson.version}`);
     },
     /**
-     * Print the short hw configuration
-     */
-    printShort: function() {
-		console.info(`H/W path\tDevice\tClass\tDescription\n${'='.repeat(43)}
-		\n/\t\ttodo\ttodo\ttodo
-		`);
-    },
-    /**
      * Print the help menu
      */
     printHelp: function() {
-		console.info(`Usage: lshw [ options ] [ --short | --version ]
-		lshw --short\nOptions:
-		-V, --version\t\tOutput lshw version
-		-s, --short\t\tvOutput summarised hw configuration as JSON
-		-html, --html\t\tOutput hw configuration as HTML
-		-json, --json\t\tOutput hw configuration as JSON`);
+		console.info(`Usage: lshw [ options ] [ --version | --html | --json]
+		lshw\nOptions:
+	-V, --version\t\tOutput lshw version
+	-html, --html\t\tOutput hw configuration as HTML
+	-json, --json\t\tOutput hw configuration as JSON`);
 	},
     /**
      * Print the main hw configuration as HTML
      */
     printHTML: function() {
-		console.info(`<html><h1>TODO</h1></html>`);
-	}
-	,
+		console.info(`<!DOCTYPE html>
+		<head>
+			<title>${core.hostname()}</title>
+			<style>
+				body { margin: 0; padding: 1rem;}
+				table { width: 100%; margin-bottom: 1rem; text-align:left; }
+				caption { text-decoration: underline; text-align:left; }
+				tr:nth-child(even) {background-color: #f2f2f2;}
+			</style>
+		</head>
+		<body>
+			<main>
+				<table>
+					<caption>Computer</caption>
+					<tbody>	
+						<tr>
+							<th>Width</th>
+						</tr>
+						<tr>
+							<td>${core.arch().arch}</td>
+						</tr>
+					</tbody>
+				</table>
+				<table>
+					<caption>Motherboard</caption>
+					<tbody>
+					<tr>
+						<th>Description</th>
+					</tr>
+					<tr>
+						<td>N/A</td>
+					</tr>
+				</table>
+				<table>
+					<caption>Memory</caption>
+					<tbody>
+					<tr>
+						<th>Size</th>
+					</tr>
+					<tr>
+						<td>${core.memory()}</td>
+					</tr>
+					</tbody>
+				</table>
+				<table>
+					<caption>CPU</caption>
+					<tbody>
+						<tr>
+							<th>Product</th>
+							<th>Vendor</th>
+							<th>Width</th>
+						</tr>
+						<tr>
+							<td>${core.cpu().product}</td>
+							<td>${core.cpu().vendor}</td>
+							<td>${core.cpu().width}</td>
+						</tr>
+					</tbody>
+				</table>
+				<table>
+					<tbody>
+						<caption>Network</caption>
+					<tbody>
+						<tr>
+							<th>Description</th>
+							<th>Logical Name</th>
+							<th>Serial</th>
+							<th>Configuration</th>
+						</tr>
+						<tr>
+						${Object.entries(core.network().interfaces).map( item => {
+							return `<td>Network interface</td>
+							<td>${item[0]}</td>
+							<td>${item[1][0].mac}</td>
+							<td>ip=${item[1][0].address}</td></tr>\n\t\t\t\t\t\t\t`
+						}).join("")}
+					</tbody>
+				</table>
+			</main>
+		</body>\n</html>`);
+	},
     /**
      * Print the main hw configuration as JSON
      */
     printJSON: function() {
-		console.info(`{}`);
+		console.info(`{ 
+	"description": "Computer", 
+	"width": "${core.arch().arch}",
+	"children": [ 
+		{ 
+			"description": "Motherboard" 
+		},
+		{ 
+			"description": "Memory", 
+			"size": "${core.memory()}"
+		},
+		{ 
+			"description": "CPU",
+			"product": "${core.cpu().product}",
+			"vendor": "${core.cpu().vendor}",
+			"width": "${core.cpu().width}"
+		},
+		{ 
+			"network": [
+				${Object.entries(core.network().interfaces).map( item => {
+				return `{ 
+					"description": "Network interface",
+					"logical-name": "${item[0]}",
+					"serial": "${item[1][0].mac}",
+					"configuration": "ip=${item[1][0].address}"
+				},\n\t\t\t\t`}).join("")}
+			]
+		}
+	]
+}`);
     }
 }
 
